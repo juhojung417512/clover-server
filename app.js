a0e56e771b7838604a1a26ea74b8fa346178a4ad
juhojung <jooho2479@gmail.com>
2020-04-09 16:09:56 +0900

    asdas

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const conn = new MYSQL(config.host, config.user, config.password, config.databas
 app.use(express.json());
 const router = require("./router")(app,conn);
 
+app.get('/', async(req,res)=>{
+    res.send("INDEX");
+});
+
 var server = app.listen(3000, function(){
     console.log("on : 3000");
 })
--- router.js
+++ router.js
@@ -1,13 +1,15 @@
 module.exports = function(app, conn){
-    app.get('/', async(req,res)=>{
-        let asd = await conn.query("SELECT * FROM user WHERE user_id = ?", ["asdasd"]);
-        console.log(asd[0]);
-        res.send("INDEX");
-    });
+    app.get("/ping", (req,res)=>{
+        return res.send(true);
+    })
     app.post('/login', (req,res)=>{
         var id = req.body.id;
         var pw = req.body.pw;
-        
-        res.send(true);
+        let user = await conn.query("SELECT * FROM user WHERE user_id = ? AND pw = ?", [id, pw]);
+        if(user.length > 0)
+            res.json({result : true});
+        else 
+            res.json({result : false});
+        return;
     });
 }
