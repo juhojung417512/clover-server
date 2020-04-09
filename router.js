c8626af81425e2acf02d95bc2cad8a4f26291558
juhojung <jooho2479@gmail.com>
2020-04-09 16:10:46 +0900

    asd

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ module.exports = function(app, conn){
     app.get("/ping", (req,res)=>{
         return res.send(true);
     })
-    app.post('/login', (req,res)=>{
+    app.post('/login', async (req,res)=>{
         var id = req.body.id;
         var pw = req.body.pw;
         let user = await conn.query("SELECT * FROM user WHERE user_id = ? AND pw = ?", [id, pw]);
