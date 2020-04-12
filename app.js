a251de6a778d033aa320e6585ed3d7eb8ef7d257
juhojung <jooho2479@gmail.com>
2020-04-12 17:37:51 +0900

    asdasd

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 var app = express();
 const MYSQL = require("./mysql_conn");
 const config = require("./config.json");
 const conn = new MYSQL(config.host, config.user, config.password, config.database);
-app.use(express.json()); 
-app.use(express.urlencoded( {extended : false } ));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 const router = require("./router")(app,conn);
 
 app.get('/', async(req,res)=>{
