9f0fffbf3acf8f02e1dc497947c18d6dbbbeddad
juhojung <jooho2479@gmail.com>
2020-04-12 17:31:41 +0900

    asdasd

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var app = express();
 const MYSQL = require("./mysql_conn");
 const config = require("./config.json");
 const conn = new MYSQL(config.host, config.user, config.password, config.database);
-app.use(express.json());
+app.use(express.json()); 
+app.use(express.urlencoded( {extended : false } ));
 const router = require("./router")(app,conn);
 
 app.get('/', async(req,res)=>{
